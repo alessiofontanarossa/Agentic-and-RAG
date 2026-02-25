566a01d79f49f07bea35c0bece7749c82adfe9a9
alessio fontanarossa <alessio.font7@gmail.com>
2026-02-26 00:42:14 +0100

    Cleaned branch master

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .qdrant_admin import QdrantAdmin
-from .my_tools import get_my_tools
-from .backend import backend_setup
-from .backend import QDRANT_COLLECTION_NAME, CHUNK_SIZE, CHUNK_OVERLAP, USER_NAME, SUBJECT
